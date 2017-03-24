c99fc9813adf239a0166c7349845c08cb438cf93
thienth32 <thienth32@gmail.com>
2017-03-24 21:12:29 +0700

    backup db

---
+++ db/1609_blog-2017-03-24_v2.sql
@@ -0,0 +1,151 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 127.0.0.1
+ Source Server Type    : MySQL
+ Source Server Version : 50505
+ Source Host           : 127.0.0.1
+ Source Database       : 1609_blog
+
+ Target Server Type    : MySQL
+ Target Server Version : 50505
+ File Encoding         : utf-8
+
+ Date: 03/24/2017 21:12:19 PM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `categories`
+-- ----------------------------
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT NULL,
+  `cate_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `feature_image` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+--  Records of `categories`
+-- ----------------------------
+BEGIN;
+INSERT INTO `categories` VALUES ('1', null, 'Thể thao', null, null, null, null), ('2', null, 'Xã hội', null, null, null, null), ('3', null, 'Giáo dục', null, null, null, null), ('4', null, 'Tin trong nước', null, null, null, null), ('5', null, 'thang nq', 'asdfafasdf', null, '2017-03-24 12:15:40', '2017-03-24 12:15:40'), ('6', '2', 'le van toan da dc update', 'asdfasdfasdfasdf', '/uploads/le-van-toan-da-dc-update-58d52459890b9.jpeg', '2017-03-24 12:36:27', '2017-03-24 13:54:00'), ('7', '4', 'Le ngoc anh', 'adsf,jasdfadsfnasdfa', '/uploads/Le-ngoc-anh-58d513b2a0c42.jpeg', '2017-03-24 12:40:18', '2017-03-24 14:01:22'), ('13', null, 'something', 'adsfasdfasdfasdf', '/uploads/something-58d51fa998a19.jpeg', '2017-03-24 13:31:21', '2017-03-24 13:31:21');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `migrations`
+-- ----------------------------
+DROP TABLE IF EXISTS `migrations`;
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+--  Records of `migrations`
+-- ----------------------------
+BEGIN;
+INSERT INTO `migrations` VALUES ('1', '2017_03_17_121522_create_user_table', '1'), ('2', '2017_03_17_121532_create_role_table', '1'), ('3', '2017_03_17_121545_create_user_role_xref_table', '1'), ('4', '2017_03_17_121556_create_category_table', '1'), ('5', '2017_03_17_121605_create_post_table', '1'), ('6', '2017_03_17_121611_create_slug_table', '1');
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `posts`
+-- ----------------------------
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE `posts` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cate_id` int(11) NOT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `content` text COLLATE utf8_unicode_ci,
+  `feature_image` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+--  Table structure for `roles`
+-- ----------------------------
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE `roles` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+--  Records of `roles`
+-- ----------------------------
+BEGIN;
+INSERT INTO `roles` VALUES ('1', 'admin', null, null), ('2', 'moderator', null, null), ('3', 'author', null, null), ('4', 'member', null, null);
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `slugs`
+-- ----------------------------
+DROP TABLE IF EXISTS `slugs`;
+CREATE TABLE `slugs` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `slug` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `entity_id` int(11) NOT NULL,
+  `entity` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `slugs_slug_unique` (`slug`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+--  Table structure for `user_role_xref`
+-- ----------------------------
+DROP TABLE IF EXISTS `user_role_xref`;
+CREATE TABLE `user_role_xref` (
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`,`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+--  Records of `user_role_xref`
+-- ----------------------------
+BEGIN;
+INSERT INTO `user_role_xref` VALUES ('1', '1', null, null), ('2', '2', null, null), ('2', '4', null, null);
+COMMIT;
+
+-- ----------------------------
+--  Table structure for `users`
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_username_unique` (`username`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+--  Records of `users`
+-- ----------------------------
+BEGIN;
+INSERT INTO `users` VALUES ('1', 'admin', 'admin@gmail.com', '$2y$10$qgSfBNWYNVH3.J1F6CbdVek8cLYnVQAhZPs7iFxbwG0j/ND.IaaEa', null, null), ('2', 'mod', 'mod@gmail.com', '$2y$10$8CwDHJ/2JDsSw7zrgCHwNOTs2mU7PC.usYoUKyvHWQZEAyoa8T3Ae', null, null);
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
